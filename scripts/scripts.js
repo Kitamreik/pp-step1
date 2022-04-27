6f19a4ea097911a33ffd00802d2d060597550b5c
kitamreik <kitdamreik@gmail.com>
2022-04-27 03:02:27 -0400

    CSS card functionality/cleaned up JS/add card templates to photogallery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,12 @@
-// Test Code 3-9-22 [does work]
-// alert("HALT OMG this is working - YAS");
+// Hamburger Menu
 
-// Hamburger Menu Psuedocode [update HTML/CSS]
-
-alert("OMG- Are you ready to see what's going on in this realm of cosmic deliciousness? If you're ready, click ok!");
-console.log("You're doing a great job, Kit");
-
-// Hamburger JS from CP 
+// alert("OMG- Are you ready to see what's going on in this realm of cosmic deliciousness? If you're ready, click ok!");
+// console.log("You're doing a great job, Kit");
 
 // DOM Event Steps:
 
 // 1. select hamburger icon --> event target
-// const hamburgerIcon = document.querySelector("hamburger");
-// const navUl = document.querySelector("nav-bar-list");
-
 const hamburgerIcon = document.querySelector(".menu");
-
 const navUl = document.querySelector(".nav-bar");
 
 // 2. event handler --> show/hide
